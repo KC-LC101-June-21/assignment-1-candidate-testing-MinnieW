8bc54787c381ba1a1c851fb35f58525f37654cc1
MinnieW <>
2021-06-03 16:41:43 +0000

    Add ;

--- candidate-testing.js
+++ candidate-testing.js
@@ -15,12 +15,12 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-candidateName = input.question("Enter your name: ")
+candidateName = input.question("Enter your name: ");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = input.question(question)
+candidateAnswer = input.question(question);
 
 }
 
@@ -28,9 +28,9 @@ function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 if (candidateAnswer === correctAnswer) {
-  console.log("Correct!")
+  console.log("Correct!");
 } else {
-  console.log("Incorrect")
+  console.log("Incorrect");
 }
 
   let grade;
@@ -42,7 +42,7 @@ if (candidateAnswer === correctAnswer) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  console.log("Welcome " + candidateName + "!")
+  console.log("Welcome " + candidateName + "!");
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
