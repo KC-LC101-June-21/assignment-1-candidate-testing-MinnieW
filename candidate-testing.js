b15730c76236acee3375d2f6efa793b752cf1b08
MinnieW <>
2021-06-03 01:59:57 +0000

    Complete part 1 of assignment

--- candidate-testing.js
+++ candidate-testing.js
@@ -42,7 +42,7 @@ if (candidateAnswer === correctAnswer) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  console.log("Welcome, " + candidateName)
+  console.log("Welcome, " + candidateName);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
